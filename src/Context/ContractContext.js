be5a432c0f15b0e07aee7370f3518b1131a11228
Kali-Design <hm_leo@hotmail.com>
2021-09-08 00:18:16 +0200

    done

---
+++ src/Context/ContractContext.js
@@ -0,0 +1,26 @@
+import { createContext } from "react";
+// import { useContract } from "web3-hooks";
+// import { daoFactoryAbi, daoFactoryAddress } from "../contracts/DaoFactoryAbi";
+// import { daoAbi } from "../contracts/DaoAbi";
+// import { governanceAbi } from "../contracts/GovernanceAbi";
+// import { managementAbi } from "../contracts/ManagementAbi";
+// import { treasuryAbi } from "../contracts/TreasuryAbi";
+
+export const ContractsContext = createContext(null);
+
+const ContractsContextProvider = ({ children }) => {
+  let daoAddress, governanceAddress, managementAddress, treasuryAddress
+  const daoFactory = useContract(daoFactoryAddress, daoFactoryAbi)
+  const dao = useContract(daoAddress, daoAbi)
+  const governance = useContract(governanceAddress, governanceAbi)
+  const management = useContract(managementAddress, managementAbi)
+  const treasury = useContract(treasuryAddress, treasuryAbi)
+
+  return (
+    <ContractsContext.Provider value={[daoFactory, dao, governance, management, treasury]}>
+      {children}
+    </ContractsContext.Provider>
+  );
+};
+
+export default ContractsContextProvider;
