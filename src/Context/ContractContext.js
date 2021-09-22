02a03722f97b3216c7d9e3b4f37e384cfa3605c5
Kali-Design <hm_leo@hotmail.com>
2021-09-22 12:12:10 +0200

    done

--- src/Context/ContractContext.js
+++ src/Context/ContractContext.js
@@ -2,7 +2,7 @@ import { createContext } from "react";
 import { useContract } from "web3-hooks";
 import { daoFactoryAbi } from "../contracts/DaoFactoryAbi";
 import { daoAbi } from "../contracts/DaoAbi";
-// import { governanceAbi } from "../contracts/GovernanceAbi";
+import { governanceAbi } from "../contracts/GovernanceAbi";
 // import { managementAbi } from "../contracts/ManagementAbi";
 // import { treasuryAbi } from "../contracts/TreasuryAbi";
 
@@ -12,12 +12,12 @@ const ContractsContextProvider = ({ children }) => {
   let daoAbi_address, daoFactory_address, governance_address, management_address, treasury_address
   const daoFactory = useContract(daoFactory_address, daoFactoryAbi)
   const dao = useContract(daoAbi_address, daoAbi)
-  //const governance = useContract(governance_address, governanceAbi)
+  const governance = useContract(governance_address, governanceAbi)
   //const management = useContract(management_address, managementAbi)
   //const treasury = useContract(treasury_address, treasuryAbi)
 
   return (
-    <ContractsContext.Provider value={[daoFactory, dao, /*governance, management, treasury*/]}>
+    <ContractsContext.Provider value={[daoFactory, dao, governance, /*management, treasury*/]}>
       {children}
     </ContractsContext.Provider>
   );
