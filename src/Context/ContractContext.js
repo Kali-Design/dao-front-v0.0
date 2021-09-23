8adc9f3772a0e9ad7435e6a6544e2659b68afc6a
Kali-Design <hm_leo@hotmail.com>
2021-09-24 00:17:21 +0200

    done

--- src/Context/ContractContext.js
+++ src/Context/ContractContext.js
@@ -4,7 +4,7 @@ import { daoFactoryAbi } from "../contracts/DaoFactoryAbi";
 import { daoAbi } from "../contracts/DaoAbi";
 import { governanceAbi } from "../contracts/GovernanceAbi";
 import { managementAbi } from "../contracts/ManagementAbi";
-// import { treasuryAbi } from "../contracts/TreasuryAbi";
+import { treasuryAbi } from "../contracts/TreasuryAbi";
 
 export const ContractsContext = createContext(null);
 
@@ -14,10 +14,10 @@ const ContractsContextProvider = ({ children }) => {
   const dao = useContract(daoAbi_address, daoAbi)
   const governance = useContract(governance_address, governanceAbi)
   const management = useContract(management_address, managementAbi)
-  //const treasury = useContract(treasury_address, treasuryAbi)
+  const treasury = useContract(treasury_address, treasuryAbi)
 
   return (
-    <ContractsContext.Provider value={[daoFactory, dao, governance, management, /*treasury*/]}>
+    <ContractsContext.Provider value={[daoFactory, dao, governance, management, treasury]}>
       {children}
     </ContractsContext.Provider>
   );
