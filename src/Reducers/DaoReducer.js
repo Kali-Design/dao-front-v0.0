3889ba7ab99b5f23fcb24d558c233718720da9a9
Kali-Design <hm_leo@hotmail.com>
2021-10-12 11:24:07 +0200

    done

--- src/Reducers/DaoReducer.js
+++ src/Reducers/DaoReducer.js
@@ -1,20 +1,32 @@
 export const daoReducer = (state, action) => {
-    switch (action.type) {
-      case "CHANGE_ACCOUNT":
-        return {
-          ...state,
-          account: action.payload,
-        }
-      case "CHANGE_ROLE":
-        return {
-          ...state,
-          role: action.payload,
-        }
-      case "CHANGE_GRANT":
-        return {
-          ...state,
-          grant: action.payload,
-        }
-      default: throw new Error(`Unsupported action type ${action.type}`);
-    }
+  switch (action.type) {
+    case "CHANGE_ACCOUNT":
+      return {
+        ...state,
+        account: action.payload,
+      }
+    case "CHANGE_ROLE":
+      return {
+        ...state,
+        role: action.payload,
+      }
+    case "CHANGE_GRANT":
+      return {
+        ...state,
+        grant: action.payload,
+      }
+    case "CHANGE_ISROLE":
+      return {
+        ...state,
+        isRole: action.payload,
+      }
+    case "UPDATE_ADDRESS":
+      return {
+        ...state,
+        governanceAddress: action.payload.governanceAddress,
+        managementAddress: action.payload.managementAddress,
+        treasuryAddress: action.payload.treasuryAddress,
+      }
+    default: throw new Error(`Unsupported action type ${action.type}`);
   }
+}
